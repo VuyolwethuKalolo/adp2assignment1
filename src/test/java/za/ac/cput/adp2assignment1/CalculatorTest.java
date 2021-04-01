eeb6e08f9b8af19d0717dd78b666d7034e3ce556
VUYOLWETHU <VUYOLWETHU@Vuolwethu>
2021-04-01 05:36:04 -0700

    Adding int numbers

---
+++ src/test/java/za/ac/cput/adp2assignment1/CalculatorTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.adp2assignment1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author VUYOLWETHU
+ */
+public class CalculatorTest {
+    
+    private Calculator calculate;
+    
+    public CalculatorTest() {
+        
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        this.calculate= new Calculator();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+    @Test
+    public void testAddition(){
+    
+    int result=calculate.addition(3 , 7);
+    assertEquals(10, result);
+    }
+    
+}
